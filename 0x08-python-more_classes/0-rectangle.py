6245f41bad67799604096dc6cbdf2ac57ac14681
Desolution01 <jas3610@gmail.com>
2023-04-03 16:03:47 -0700

    task done

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This is for your readability. this  task tells us more about  a class - Rectangle
+"""
+
+
+class Rectangle:
+    """
+    This is an empty class
+    """
+    pass
